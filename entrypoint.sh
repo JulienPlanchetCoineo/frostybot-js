7b7e2828f578bd5a81d78bf1caabb06b3dc99e03
CryptoMF <andrew.rall@gmail.com>
2020-12-13 14:40:00 +0200

    Updated MOTD cron job

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ echo "$SSH_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 cp /root/.profile /usr/local/frostybot-js/.profile
 chown $SSH_USER /usr/local/frostybot-js/.profile
 echo "PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@frostybot\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\\$ '" > /usr/local/frostybot-js/.bashrc
+echo "PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@frostybot\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\\$ '" >> /root/.bashrc
 chown $SSH_USER /usr/local/frostybot-js/.bashrc
 sed -i "/.*Port .*/cPort $SSH_PORT" /etc/ssh/sshd_config
 echo $FROSTYBOT_PORT > /usr/local/frostybot-js/.port
