d2d44f8755b5b47027808da6bcec657757f5a8c1
CryptoMF <andrew.rall@gmail.com>
2020-12-13 06:06:36 +0200

    Entrypoint bugfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,6 @@ adduser $SSH_USER sudo
 echo $SSH_USER:$SSH_PASS | chpasswd
 echo "$SSH_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 sed -i "/.*Port .*/cPort $SSH_PORT" /etc/ssh/sshd_config
-sed -i "/URL=.*/cURL=\"http://127.0.0.1:$FROSTYBOT_PORT/frostybot\"" /usr/local/frostybot-js/frostybot
-sed -i "/var port = .*/cvar port = $FROSTYBOT_PORT;" /usr/local/frostybot-js/bin/www
 
 service ssh start
 frostybot start
