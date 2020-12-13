513072bec74e13b6fd1b4f8cbed8a6bba716972c
CryptoMF <andrew.rall@gmail.com>
2020-12-13 10:22:32 +0200

    Improved container console prompt

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ useradd -s /bin/bash -d /usr/local/frostybot-js $SSH_USER
 adduser $SSH_USER sudo
 echo $SSH_USER:$SSH_PASS | chpasswd
 echo "$SSH_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+cp /root/.profile /usr/local/frostybot-js/.profile
+chown $SSH_USER /usr/local/frostybot-js/.profile
+echo "PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@FrostyBot\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\\$ '" > /usr/local/frostybot-js/.bashrc
+chown $SSH_USER /usr/local/frostybot-js/.bashrc
 sed -i "/.*Port .*/cPort $SSH_PORT" /etc/ssh/sshd_config
 echo $FROSTYBOT_PORT > /usr/local/frostybot-js/.port
 
