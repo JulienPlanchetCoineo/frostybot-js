d433a833cd5a9e7e31625be50d0c670cb9fb6770
CryptoMF <andrew.rall@gmail.com>
2020-12-13 14:15:14 +0200

    Updated MOTD cron job

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ echo "PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@frostybot\[\033
 chown $SSH_USER /usr/local/frostybot-js/.bashrc
 sed -i "/.*Port .*/cPort $SSH_PORT" /etc/ssh/sshd_config
 echo $FROSTYBOT_PORT > /usr/local/frostybot-js/.port
-wget https://tinyurl.com/frostybot-cron -O /etc/cron.daily/cron.motd
+wget https://tinyurl.com/frostybot-cron -O /etc/cron.daily/cron.motd  > /dev/null 2>&1
 chmod +x /etc/cron.daily/cron.motd
 `/etc/cron.daily/cron.motd`
 
