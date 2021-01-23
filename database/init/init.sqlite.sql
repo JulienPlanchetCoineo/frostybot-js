a0fb6aa9c05baf047309ae4204976848d557a6ee
CryptoMF <cryptomf@frostytrading.com>
2021-01-23 03:18:16 +0200

    Database Updates

--- database/init/init.sqlite.sql
+++ database/init/init.sqlite.sql
@@ -13,8 +13,9 @@ CREATE TABLE IF NOT EXISTS settings (
 REPLACE INTO "settings" ("mainkey", "subkey", "value") VALUES
 	('core', 'build', '1'),
 	('core', 'language', '"en"'),
-	('whitelist', '52.32.178.7', '{"ip":"52.32.178.7","description":"TradingView Server Address","canDelete":0}'),
-	('whitelist', '54.218.53.128', '{"ip":"54.218.53.128","description":"TradingView Server Address","canDelete":0}'),
-	('whitelist', '34.212.75.30', '{"ip":"34.212.75.30","description":"TradingView Server Address","canDelete":0}'),
-	('whitelist', '52.89.214.238', '{"ip":"52.89.214.238","description":"TradingView Server Address","canDelete":0}'),
-	('whitelist', '127.0.0.1', '{"ip":"127.0.0.1","description":"localhost","canDelete":0}'),
+	('whitelist', '52.32.178.7', '{"ipAddress":"52.32.178.7","description":"TradingView Server Address","canDelete":0}'),
+	('whitelist', '54.218.53.128', '{"ipAddress":"54.218.53.128","description":"TradingView Server Address","canDelete":0}'),
+	('whitelist', '34.212.75.30', '{"ipAddress":"34.212.75.30","description":"TradingView Server Address","canDelete":0}'),
+	('whitelist', '52.89.214.238', '{"ipAddress":"52.89.214.238","description":"TradingView Server Address","canDelete":0}'),
+	('whitelist', '127.0.0.1', '{"ipAddress":"127.0.0.1","description":"localhost","canDelete":0}'),
+	('whitelist', '::1', '{"ipAddress":"::1","description":"localhost","canDelete":0}');
