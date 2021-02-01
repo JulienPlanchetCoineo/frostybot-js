e71f71afe002fb10ba334a2fffe8bbd2df23903d
CryptoMF <cryptomf@frostytrading.com>
2021-02-01 16:00:10 +0200

    Minor updates

--- database/updates/update.sqlite.1.sql
+++ database/updates/update.sqlite.1.sql
@@ -1,5 +1,5 @@
 -- Version 1
-ALTER TABLE `users` ADD COLUMN `2fa` VARCHAR(20) DEFAULT 'false';
+ALTER TABLE `users` ADD COLUMN `2fa` VARCHAR(40) DEFAULT 'false';
 
 -- Update version
 INSERT OR REPLACE INTO `settings` (mainkey, subkey, value) VALUES ('core', 'sqlite:dbver', 2);
