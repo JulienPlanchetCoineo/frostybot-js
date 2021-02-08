3fd022add5a1aac290662f619675c07f4b1926dd
CryptoMF <cryptomf@frostytrading.com>
2021-02-08 20:04:34 +0200

    GUI Bugfixes

--- database/updates/update.mysql.0.sql
+++ database/updates/update.mysql.0.sql
@@ -1,15 +1,19 @@
-
+--
 -- Create logs table
+--
 
 CREATE TABLE IF NOT EXISTS `logs` (
-	`uid` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `uuid` CHAR(36) NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000',
-	`timestamp` DATETIME NOT NULL DEFAULT current_timestamp,
-	`type` VARCHAR(10) NOT NULL,
-	`message` TEXT NOT NULL,
-	PRIMARY KEY (`uid`),
-	INDEX `IDX_UUID_TS` (`uuid`, `timestamp`)
-) COLLATE='latin1_swedish_ci';
+  `uid` int unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000',
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `type` varchar(10) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`uid`),
+  KEY `IDX_UUID_TS` (`uuid`,`timestamp`)
+) ENGINE=InnoDB AUTO_INCREMENT=9434 DEFAULT CHARSET=latin1;
 
+--
 -- Update version
-REPLACE INTO `settings` (mainkey, subkey, value) VALUES ('core', 'mysql:dbver', '1');
+--
+
+REPLACE INTO `settings` (`mainkey`, `subkey`, `value`) VALUES ('core', 'mysql:dbver', '1');
